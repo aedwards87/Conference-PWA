58eb4df06d7f1c7f7685bc777733322a7cf54a0e
Adam Edwards <aedwards1987@gmail.com>
2019-07-12 16:39:19 +0100

    some changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -16,8 +16,8 @@ const Home = () => (
         matches ? (
           <Spring
             native
-            from={{ opacity: 0, transform: 'translate3d(0,40px,0)' }}
-            to={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+            from={{ opacity: 0, transform: 'translate3d(0,-40vh,0)'}}
+            to={{ opacity: 1, transform: 'translate3d(0,-5vh,0)' }}
             config={{ tension: 100, friction: 10 }}
           >
             {props => (
